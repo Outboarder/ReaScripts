8f9c62a9885e685fffc5b684f965d8e69abfdab3
Outboarder <pooyapayvar@gmail.com>
2017-06-22 11:15:31 +0430

    Add files via upload

---
+++ Track/Outboarder - Previous Input Channel.lua
@@ -0,0 +1,29 @@
+--[[
+Description: Previous Input Channel 
+Instructions:
+Screenshot:
+Version: 1.0
+Author: Outboarder
+Changelog:
+REAPER: 5.40
+Licence: GPL v3
+--]]
+
+--[[
+Changelog:
++ Initial Release v1.0 (2017-04-01)
+--]]
+
+Count_Tracks = reaper.CountSelectedTracks(0)
+for i = 0 ,Count_Tracks-1 do
+  
+    Media_Track = reaper.GetSelectedTrack(0,i) 
+    if Media_Track ~= nil then
+    Input_Mono = reaper.GetMediaTrackInfo_Value( Media_Track, 'I_RECINPUT' )
+    reaper.SetMediaTrackInfo_Value( Media_Track, 'I_RECINPUT' , Input_Mono-1 )    
+    --reaper.SetMediaTrackInfo_Value( Media_Track, 'I_RECINPUT' , Input_Stereo+1023 )  
+  end
+end
+                                 
+                 
+
